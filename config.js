534c7c68bcede7a3c00f3b7ff1f8864028c09580
Jamie12-le <le1289355@gmail.com>
2024-10-19 23:38:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Thoa';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng bạn';
-const contentCard = 'AAA mặc dù hong là dì của nhau nhưng mà nhân ngày 20/10 Chúc Thoa luôn vui vẻ hạnh phúc, ngày càng xinh đẹp, luôn may mắn, thành công cuộc đời luôn rực rỡ không chỉ hôm nay mà cho tới hết cả cuộc đời';
+const contentCard = 'AAA mặc dù hong là dì của nhau nhưng mà nhân ngày 20/10 Chúc Thoa luôn vui vẻ hạnh phúc, ngày càng xinh đẹp, luôn may mắn, thành công cuộc đời luôn rực rỡ không chỉ hôm nay mà cho tới hết cả cuộc đời nha';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
